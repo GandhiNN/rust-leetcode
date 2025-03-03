a99af8bcffddb57d75f414e41908ca1d3b5607e6
Ngakan Gandhi <ngandhi@pmintl.net>
2025-03-03 08:46:31 +0700

    add more example

--- problem_set/faangshui/print_diamond/src/lib.rs
+++ problem_set/faangshui/print_diamond/src/lib.rs
@@ -1,7 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 /* Create a function to print this
 diamond using '*' characters, given an
 odd number `n` that specifies the width
@@ -18,14 +14,37 @@ Expected output:
   *
 
 */
+pub fn print_diamond(n: &i32) {
+    // n must be an odd number
+    if n % 2 == 0 {
+        return;
+    }
+    // a diamond has n x n dimension
+    // where middle index has n number of *
+    for i in 1..*n + 1 {
+        let mut j = i - (*n / 2 + 1);
+        if j < 0 {
+            j *= -1;
+        }
+        // diamond print logic
+        print!("{}", " ".repeat(j as usize));
+        print!("{}", "*".repeat((*n - (j * 2)) as usize));
+        print!("{}", " ".repeat(j as usize));
+        println!();
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_print_diamond_5() {
+        print_diamond(&5);
+    }
+
+    #[test]
+    fn test_print_diamond_9() {
+        print_diamond(&9);
     }
 }
